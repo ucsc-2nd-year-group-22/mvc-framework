b61003cfe9e4eda684b5fa7aa724c3dfe82a9a77
ddhanushka <dimuthudhanushka8@gmail.com>
2020-10-05 17:50:14 +0530

    Move db settings from libs to config directory

--- libs/Database.php
+++ libs/Database.php
@@ -2,7 +2,6 @@
 class Database extends PDO {
 
     public function __construct() {
-        parent::__construct('mysql:host=localhost; dbname=mvc', 'ddhash', '1234');
-
+        parent::__construct(DB_TYPE. ':host='. DB_HOST .';dbname='. DB_NAME ,DB_USER, DB_PASS);
     }
 }

1081bd3908ca966ba8badd727a5a90c54e59e960
ddhanushka <dimuthudhanushka8@gmail.com>
2020-10-07 15:49:21 +0530

    Link jquery and custome.js inside footer.php

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
     <div id="footer">
     footer
     </div>
-    <script src="<?= URL ?>public/js/custom.js"/>
-
+    <script src="<?php echo URL;?>public/js/custom.js"></script>
+    <script src="<?php echo URL;?>public/js/jquery-3.5.1.min.js"></script>
 </body>
 </html>

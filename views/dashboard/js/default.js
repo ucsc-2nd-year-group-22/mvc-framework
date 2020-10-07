2dbc28c01ca80be2f2cf695204711de33beba6ce
ddhanushka <dimuthudhanushka8@gmail.com>
2020-10-07 17:50:24 +0530

    Show [x] on front of texts using jquery

--- views/dashboard/js/default.js
+++ views/dashboard/js/default.js
@@ -5,8 +5,20 @@ $(function(){
         // console.log(o[0].text);
 
         for(var i = 0; i<o.length; i++) {
-            $('#listInserts').append('<div>' + o[i].text + '<a class="del" href="#"> [X]</a></div>');
+            $('#listInserts').append('<div>' + o[i].text + '<a class="del" rel="'+ o[i].id + '" href="#"> [X]</a></div>');
         }
+
+        $('.del').click(function() {
+            var id = $(this).attr('rel');
+            // alert(id);
+
+            $.post(url, data, function(o) {
+                $('#listInserts').append('<div>'+ o.text + '<a class="del" rel="' + o.id + '" href="#"> [X]</a></div>');
+            }, 'json');
+
+            return false;
+
+        });
 
     }, 'json');
     
@@ -17,14 +29,11 @@ $(function(){
 
         $.post(url, data, function(o) {
             // console.log(o);
-            $('#listInserts').append('<div>'+ o.text + '<a class="del" rel="'+o.id+'" href="#"> [X]</a></div>');
+            $('#listInserts').append('<div>'+ o.text + '<a class="del" rel="' + o.id + '" href="#"> [X]</a></div>');
         }, 'json');
 
         return false;
     });
 
-    $('.del').click(function() {
-
-        return false;
-    });
+    
 });
